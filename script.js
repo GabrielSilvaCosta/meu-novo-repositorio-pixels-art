a376590203f4f327f2bf935bf0bc3520f0ca6b6c
GabrielSilvaCosta <bob@>
2023-02-06 20:04:25 -0300

    inicinado a 5

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ for (let i = 1; i <= 25; i += 1) {
   blocke.textContent = i;
   container.appendChild(blocke);
 }
+
+const button = document.getElementById('button-random-color');
+// eslint-disable-next-line no-use-before-define
+button.addEventListener('click', generateRandom);
+function generateRandom() {
+  const colorcaixa = document.querySelectorAll('.color');
+  colorcaixa[0].style.backgroundColor = 'black';
+  for (let index = 1; index < colorcaixa.length; index += 1) {
+    // eslint-disable-next-line no-use-before-define
+    colorcaixa[index].style.backgroundColor = getRandomColor();
+  }
+}
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i += 1) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
