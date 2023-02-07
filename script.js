e6bd1ede48e10af7c51119cf8ac9450f6c97931b
GabrielSilvaCosta <bob@>
2023-02-06 21:11:31 -0300

    fazendo a 6

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const container = document.getElementById('container');
-for (let i = 1; i <= 25; i += 1) {
-  const blocke = document.createElement('div');
-  blocke.classList.add('blocke');
-  blocke.textContent = i;
-  container.appendChild(blocke);
-}
-
 const button = document.getElementById('button-random-color');
 // eslint-disable-next-line no-use-before-define
 button.addEventListener('click', generateRandom);
@@ -24,4 +16,38 @@ function getRandomColor() {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
+}
+//---------------------------------------------------------------------------------------
+
+// eslint-disable-next-line no-unused-vars
+function generateColorPalette() {
+  const colorPalette = [];
+  for (let i = 0; i < 5; i += 1) {
+    colorPalette.push(getRandomColor());
+  }
+  localStorage.setItem('colorPalette', JSON.stringify(colorPalette));
+  return colorPalette;
+}
+
+// eslint-disable-next-line no-unused-vars, sonarjs/no-identical-functions
+function getColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i += 1) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+let savedColorPalette = JSON.parse(localStorage.getItem('colorPalette'));
+if (!savedColorPalette) {
+  savedColorPalette = generateColorPalette();
+}
+//----------------------------------------------------------------------
+// eslint-disable-next-line no-unused-vars
+const pixelBoard = document.getElementById('pixel-board');
+for (let index = 0; index < 25; index += 1) {
+  const pixel = document.createElement('div');
+  pixel.classList.add('pixel');
+  pixelBoard.appendChild(pixel);
 }
