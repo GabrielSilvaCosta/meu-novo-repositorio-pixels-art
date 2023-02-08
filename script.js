566a08bfbe226ed77c04bd1144ad941119b005e0
GabrielSilvaCosta <bob@>
2023-02-08 09:53:27 -0300

    refazendo a 5

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-shadow */
+
+// atividade 4.
 const button = document.getElementById('button-random-color');
 // eslint-disable-next-line no-use-before-define
 button.addEventListener('click', generateRandom);
@@ -24,20 +27,28 @@ function getRandomColor() {
 }
 //---------------------------------------------------------------------------------------
 
-// eslint-disable-next-line no-unused-vars
 // questao 5
-// eslint-disable-next-line no-unused-vars
-const restoreStorage = () => {
-  const paletes = document.getElementsByClassName('color');
-  const restauraColor = JSON.parse(localStorage.getItem('colorPalette'));
-  if (restauraColor) {
-    for (let index = 1; index < paletes.length; index += 1) {
-      paletes[index].style.backgroundColor = restauraColor[index];
+const localStorage = () => {
+  const colorPalette = {
+    0: black[0].style.backgroundColor,
+    1: red[1].style.backgroundColor,
+    2: blue[2].style.backgroundColor,
+    3: green[3].style.backgroundColor,
+  };
+  localStorage.setItem('color-palette', JSON.stringify(colorPalette));
+};
+
+const restore = () => {
+  const peleta = document.getElementById('#color-palette');
+  const restaure = JSON.parse(localStorage.getItem('color-palette'));
+  if (restaure) {
+    for (let index = 1; index < peleta.length; index += 1) {
+      peleta[index].style.backgroundColor = restaure[index];
     }
   }
 };
 //----------------------------------------------------------------------
-// eslint-disable-next-line no-unused-vars
+// atividade 6
 const pixelBoard = document.getElementById('pixel-board');
 for (let index = 0; index < 25; index += 1) {
   const pixel = document.createElement('div');
@@ -48,8 +59,7 @@ for (let index = 0; index < 25; index += 1) {
 const black = document.querySelector('.black');
 black.classList.add('selected');
 //----------------------------------------------------------------------
-// eslint-disable-next-line no-unused-vars
-// 9
+// atividade 9
 const selectColor = () => {
   const coloresPalette = document.querySelectorAll('.color');
   for (let index = 0; index < coloresPalette.length; index += 1) {
